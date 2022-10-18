913e13fa8a1c71e71fca9d69e6c7d87ad353cbea
changbokLee <changbok2453@gmail.com>
2022-10-18 16:51:37 +0900

    ff

---
+++ 15.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+void myFunction(){
+    printf("It's my function\n");
+}
+
+void yourFunction(){
+    printf("It's your function \n");
+}
+
+int main(void)
+{
+    void(*fp)() =  myFunction;
+    fp();
+    fp = yourFunction;
+    fp();
+    return 0;
+}
