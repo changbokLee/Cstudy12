dfb3c3a44c2441c19c187a9b05955ac114d2954e
changbokLee <changbok2453@gmail.com>
2022-10-18 14:57:49 +0900

    dd

--- Cstudy12/10.c
+++ Cstudy12/10.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 #include <stdio.h>
 #include <limits.h>
@@ -10,4 +17,13 @@ int main(void)
        if (maxValue <a[i]) maxValue = a[i];
    }
  printf("%d\n", maxValue);
+  
+  int c = 5; 
+  int *d = &c;
+  printf("%d\n" , *d);
+  
+  char a= get char
+ 
+  
+  return 0 ;
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Cstudy12
 
 C C++ C#공부
+
+
+<img src = "CSTUDY12-1/img.jpg">
